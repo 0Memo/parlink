b867904be1a94577c20e0d1f9399ebe0aa0eadd0
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-02-11 21:05:24 +0100

    updated settings

--- src/components/Navbar/NavbarBanner.tsx
+++ src/components/Navbar/NavbarBanner.tsx
@@ -9,17 +9,17 @@ import '../../App.css';
 export function NavbarBanner() {
     return (
         <Banner>
-            <div className="banner flex h-48 sm:h-24 w-full justify-between border-b border-gray-200 bg-gray-50 p-4 dark:border-gray-600">
+            <div className="banner flex max-h-64 sm:h-24 w-full justify-between border-b border-gray-200 bg-gray-50 p-4 dark:border-gray-600">
                 <div className="mx-auto flex flex-col items-center">
                     <p className="flex items-center text-sm font-h1 text-gray-500 dark:text-gray-400 mb-4 sm:mb-2">
                         <HiMiniWrench className="mr-4 h-10 sm:h-6 w-10 sm:w-6 bannerIcon" />
                         <span className="[&_p]:inline text-lg bannerText">
                             Application en cours de développment
                         </span>
-                        <HiMiniWrench className="ml-4 h-16 sm:h-6 w-16 sm:w-6 bannerIcon" />
+                        <HiMiniWrench className="ml-4 h-10 sm:h-6 w-10 sm:w-6 bannerIcon" />
                     </p>
                     <p className="flex items-center text-sm font-h1 text-gray-500 dark:text-gray-400">
-                        <MdAnnouncement className="mr-4 h-6 w-6 bannerIcon" />
+                        <MdAnnouncement className="mr-4 h-16 sm:h-6 w-16 sm:h-6 bannerIcon" />
                         <span className="[&_p]:inline text-lg bannerText">
                             Certaines fonctionnalités peuvent ne pas être finalisées. Merci pour votre compréhension !
                         </span>
