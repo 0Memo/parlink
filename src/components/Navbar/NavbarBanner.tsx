587e644d7ffc24adb49ce9b498daef0ba87010c8
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-02-11 20:43:03 +0100

    latest updates

--- src/components/Navbar/NavbarBanner.tsx
+++ src/components/Navbar/NavbarBanner.tsx
@@ -9,14 +9,14 @@ import '../../App.css';
 export function NavbarBanner() {
     return (
         <Banner>
-            <div className="banner flex h-24 sm:h-16 w-full justify-between border-b border-gray-200 bg-gray-50 p-4 dark:border-gray-600">
+            <div className="banner flex h-48 sm:h-24 w-full justify-between border-b border-gray-200 bg-gray-50 p-4 dark:border-gray-600">
                 <div className="mx-auto flex flex-col items-center">
-                    <p className="flex items-center text-sm font-h1 text-gray-500 dark:text-gray-400">
-                        <HiMiniWrench className="mr-4 h-6 w-6 bannerIcon" />
+                    <p className="flex items-center text-sm font-h1 text-gray-500 dark:text-gray-400 mb-4 sm:mb-6">
+                        <HiMiniWrench className="mr-4 h-10 sm:h-6 w-10 sm:w-6 bannerIcon" />
                         <span className="[&_p]:inline text-lg bannerText">
                             Application en cours de développment
                         </span>
-                        <HiMiniWrench className="mr-4 h-6 w-6 bannerIcon" />
+                        <HiMiniWrench className="ml-4 h-16 sm:h-6 w-16 sm:w-6 bannerIcon" />
                     </p>
                     <p className="flex items-center text-sm font-h1 text-gray-500 dark:text-gray-400">
                         <MdAnnouncement className="mr-4 h-6 w-6 bannerIcon" />
