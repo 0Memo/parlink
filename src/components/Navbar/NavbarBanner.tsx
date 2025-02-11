1882660b393891dbd1a2593dde6c6b8d6c5262b6
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-02-11 20:30:49 +0100

    updated NavbarBanner.tsx

--- src/components/Navbar/NavbarBanner.tsx
+++ src/components/Navbar/NavbarBanner.tsx
@@ -3,6 +3,7 @@
 import { Banner } from "flowbite-react";
 import { HiX } from "react-icons/hi";
 import { MdAnnouncement } from "react-icons/md";
+import { HiMiniWrench } from "react-icons/hi2";
 import '../../App.css';
 
 export function NavbarBanner() {
@@ -11,9 +12,16 @@ export function NavbarBanner() {
             <div className="banner flex h-24 sm:h-16 w-full justify-between border-b border-gray-200 bg-gray-50 p-4 dark:border-gray-600">
                 <div className="mx-auto flex items-center">
                     <p className="flex items-center text-sm font-h1 text-gray-500 dark:text-gray-400">
+                        <HiMiniWrench className="mr-4 h-6 w-6 bannerIcon" />
+                        <span className="[&_p]:inline text-lg bannerText">
+                            Application en cours de développment
+                        </span>
+                        <HiMiniWrench className="mr-4 h-6 w-6 bannerIcon" />
+                    </p>
+                    <p className="flex items-center text-sm font-h1 text-gray-500 dark:text-gray-400">
                         <MdAnnouncement className="mr-4 h-6 w-6 bannerIcon" />
                         <span className="[&_p]:inline text-lg bannerText">
-                            Attention! Ceci est un projet, cette application n'est pas disponible pour l'instant
+                            Certaines fonctionnalités peuvent ne pas être finalisées. Merci pour votre compréhension !
                         </span>
                     </p>
                 </div>
